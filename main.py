725aef016403670724ff6ce4c1d9076554686aec
Francisco Antilef <francisco.antilef.jaramillo@gmail.com>
2018-05-07 15:18:08 -0300

    estaba todo bien hasta que me acorde de la epsilon     solo falta definir ese detalle

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from DataStruct.Stack import Stack
 from DataStruct.Queue import Queue
 
-transiciones = Queue()
+transiciones = []
 pilaMemoria = Stack()
 estadoInicial = ""
 palabraEntrada = ""
+colaEntrada = Queue()
 estadoFinal = ""
 def transicion_Mala(tran):
-    return tran[0]!='(' or tran[2]!=',' or tran[4]!=',' or tran[6]!=')' or tran[7]!='=' or tran[8]!='(' or tran[10]!=',' or tran[12]!=')'
+    ##hay que modificar esto
+    return tran[0]!='(' or tran[2]!=',' or tran[4]!=',' or tran[6]!=')' or tran[7]!='=' or tran[8]!='(' or tran[10]!=',' or tran[len(tran)-1]!=')'
         
 def pide_transiciones(transiciones):
     tran=str(input("Ingrese las transiciones (presiones ENTER para terminar):"))
@@ -16,7 +18,7 @@ def pide_transiciones(transiciones):
         while(transicion_Mala(tran)):
             tran=str(input("Error..Ingrese las transiciones otra vez:"))
             tran=tran.replace(' ','')
-        transiciones.encolar(tran)
+        transiciones.append(tran)
         tran=str(input("Ingrese las transiciones (presiones ENTER para terminar):"))
         tran=tran.replace(' ','')
 
@@ -36,26 +38,57 @@ def validaEntrada(mensaje):
         estado=str(input("Error..."+mensaje))
         estado=estado.replace(' ','')
     return estado
-"""
-def apd_stack_vacio(transiciones,estadoInicial,palabraEntrada,pilaMemoria):
+def buscar_transicion(transiciones,estadoActual,sim,variableStack):
+    pos=0
+    while pos<len(transiciones):
+        if transiciones[pos][1]==estadoActual and transiciones[pos][3]==sim and transiciones[pos][5]==variableStack :
+            return transiciones[pos]
+        pos = pos + 1
+    return ""
+def apilado(pilaMemoria,tran):
+    pos=tran[11]
+    while tran[pos]!=")":
+        if tran[pos]="E":
+            r=pilaMemoria.desapilar()
+        else:
+            pilaMemoria.apilar(tran[pos])
+        pos = pos + 1
+
+def apd_stack_vacio(transiciones,estadoInicial,colaEntrada,pilaMemoria):
     estadoActual= estadoInicial 
-    simbolo=0
-    while(len(palabraEntrada)>0):
-        if()
-    return True
-"""
+    sim = colaEntrada.desencolar()
+    while(sim!=""):
+        variableStack=pilaMemoria.desapilar()
+        tran=buscar_transicion(transiciones,estadoActual,sim,variableStack)
+        if(tran!=""):
+            estadoActual=tran[1]
+            apilado(pilaMemoria,tran)
+        else:
+            print("no existe la transicion ")
+            return           
+        sim = colaEntrada.desencolar()
+    if(pilaMemoria.es_vacia()):
+        print("esta terrible weno el apd SI SI SI")
+    else:
+        print("NO NO NO")
+def crearPalabra(palabraEntrada,colaEntrada):
+    pos=0
+    while pos<len(palabraEntrada):
+        colaEntrada.encolar(palabraEntrada[pos])
+        pos = pos + 1
 def main():
-    print("Bienvenido a nuesta super tarea salvaje #MuerteAlHeinz")
+    print("Bienvenido a nuesta super tarea salvaje(la epsilon=E ) #MuerteAlHeinz")
     pilaMemoria.apilar("R")
     pide_transiciones(transiciones)
-    if(transiciones.es_vacia()):
+    if transiciones==[]:
         print("Error")
     else:
         estadoInicial = validaEntrada("Ingrese el estado Inicial : ")
-        palabraEntrada = validaEntrada("Ingrese la palabra de entrada : ") 
+        palabraEntrada = validaEntrada("Ingrese la palabra de entrada : ")
+        crearPalabra(palabraEntrada,colaEntrada)
         if(por_stack_vacio() ):
             """
-            if(apd_stack_vacio(transiciones,estadoInicial,palabraEntrada,pilaMemoria)):
+            if(apd_stack_vacio(transiciones,estadoInicial,colaEntrada,pilaMemoria)):
                 print("La palabra es aceptado por el APD por stack vacio")
             else:
                 print("no se llama ") 
@@ -63,7 +96,7 @@ def main():
         else:
             estadoFinal=validaEntrada("Ingrese el estado Final : ")
             """
-            if(apd_estado_final(transiciones,estadoInicial,palabraEntrada,estado_final,pilaMemoria)):
+            if(apd_estado_final(transiciones,estadoInicial,colaEntrada,estado_final,pilaMemoria)):
                 print("La palabra es aceptado por el APD por estado_final")
             else:
                 print("no se llama ")
--- test.py
+++ test.py
@@ -1,5 +1,10 @@
 print("pruebas idiotas ")
-if "A"=='A':
-    print("sabpee")
-else:
-    print("mala vola")
+transiciones = ["(1,1,1)=(1,1)","(2,2,2)=(2,2)"]
+def buscar_transicion(transiciones,estadoActual,sim,variableStack):
+    pos=0
+    while pos<len(transiciones):
+        if transiciones[pos][1]==estadoActual and transiciones[pos][3]==sim and transiciones[pos][5]==variableStack :
+            return transiciones[pos]
+        pos = pos + 1
+    return ""
+print(buscar_transicion(transiciones,"1","","1"))
